aafbd5d4b21f12136f19d5e3fff8e7027247730e
SRuedesh <39556768+SRuedesh@users.noreply.github.com>
2024-10-02 15:24:26 +0200

    Changes 10/2024 - removed lualine

--- init.lua
+++ init.lua
@@ -1007,32 +1007,32 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-    config = function()
-      require('lualine').setup {
-        options = { theme = 'rose-pine' },
-        sections = {
-          lualine_a = { 'mode' },
-          lualine_b = { 'branch' },
-          lualine_c = { 'filename' },
-          lualine_x = { 'encoding', 'fileformat', 'filetype' },
-          lualine_y = { 'progress' },
-          lualine_z = { 'location' },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { 'filename' },
-          lualine_x = { 'location' },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        extensions = { 'nvim-tree' },
-      }
-    end,
-  },
+  -- {
+  --   'nvim-lualine/lualine.nvim',
+  --   dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+  --   config = function()
+  --     require('lualine').setup {
+  --       options = { theme = 'rose-pine' },
+  --       sections = {
+  --         lualine_a = { 'mode' },
+  --         lualine_b = { 'branch' },
+  --         lualine_c = { 'filename' },
+  --         lualine_x = { 'encoding', 'fileformat', 'filetype' },
+  --         lualine_y = { 'progress' },
+  --         lualine_z = { 'location' },
+  --       },
+  --       inactive_sections = {
+  --         lualine_a = {},
+  --         lualine_b = {},
+  --         lualine_c = { 'filename' },
+  --         lualine_x = { 'location' },
+  --         lualine_y = {},
+  --         lualine_z = {},
+  --       },
+  --       extensions = { 'nvim-tree' },
+  --     }
+  --   end,
+  -- },
   {
     -- Pomodoro Timer
     'epwalsh/pomo.nvim',
